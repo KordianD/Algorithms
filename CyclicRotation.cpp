ffaaeb9ea3cf56dc284f8af410035bec27def66c
KordianD <KordianD@users.noreply.github.com>
2016-12-22 21:54:56 +0100

    Code for Codility (CyclicRotation)

---
+++ CyclicRotation.cpp
@@ -0,0 +1,21 @@
+// Code was writen for Codility
+
+vector<int> solution(vector<int> &A, int K) {
+
+    unsigned int howManyMoves = K % A.size();
+
+    vector<int> temp(A.size());
+
+    for (unsigned int i = 0; i < A.size(); i++){
+        if (i + howManyMoves >= A.size())
+            temp.at(i + howManyMoves - A.size()) = A[i];
+        else
+            temp.at(i + howManyMoves) = A[i];
+    }
+
+    for (unsigned int i = 0; i < A.size(); i++)
+        A[i] = temp[i];
+
+    return A;
+
+}
