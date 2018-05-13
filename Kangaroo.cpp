8cc9b60292ca992d5c7cb3b3db0fd17b1ac6e1c2
KordianD <kordianD>
2018-05-13 10:28:59 +0200

    Kangaroo done

---
+++ Kangaroo.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    float x1, x2;
+    int v1, v2;
+    std::cin >> x1 >> v1 >> x2 >> v2;
+    if (v2 >= v1)
+    {
+        std::cout << "NO";
+        return 0;
+    }
+    
+    auto n = (x2-x1)/(v1-v2) - int((x2-x1)/(v1-v2));
+    if (n > 0)
+        std::cout << "NO";
+    else
+        std::cout << "YES";
+    
+    return 0;
+}
+
