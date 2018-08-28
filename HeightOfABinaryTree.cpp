264415876e19f60b39b898dc32f1460038cec54a
KordianD <kordiandrozd@gmail.com>
2018-08-28 19:00:32 +0200

    HeightOfABinaryTree done

---
+++ HeightOfABinaryTree.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/*The tree node has data, left child and right child
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+     if (root == nullptr)
+     {
+        return 0;
+     }
+
+     if (root->left == nullptr and root->right == nullptr)
+     {
+         return 0;
+     }
+
+
+     return height(root->left) + 1 >  height(root->right) + 1 ? height(root->left) + 1: height(root->right) + 1;
+
+
+    }
+
+}; //End of Solution
+
+int main() {
+
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    int height = myTree.height(root);
+    std::cout << height;
+
+    return 0;
+}
+
